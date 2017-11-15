178f11359e2e0c14a9000e79b1451bd853133944
at-yenho <yen.ho@asiantech.vn>
2017-11-15 11:29:44 +0700

    use fetch to get data and map to loop create element

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       React.createElement('h1', {style : {color:"red"}}, 'App')//<h1 style={{color: "red"}}>App</h1>
-//     );
-//   }
-// }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+        items: [{"name": "ANG", "age": 20}, {"name": "HsG", "age": 20}, {"name": "KyT", "age": 20}],
+        filter:false
+      }
+  }
+  componentWillMount() {
+    fetch("https://swapi.co/api/people/?format=json")
+      .then(response => response.json())
+      .then( ({results: items}) => this.setState({items}) )
+  } 
+  filter(e){
+    this.setState({filter: e.target.value});
+  }
+  render() {
+    let items = this.state.items;
+    // OR this.state.items.slice() to ceasy for later push pop items if have
+    if (this.state.filter) {
+      items = items.filter( item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()) )
+    }
+    return (
+      <div>
+        <input type="text" onChange={this.filter.bind(this)} />
+         {items.map(item => <Person person = {item} key = {item.name}/>)}
+      </div>
+    );
+  }
+}
 
-// function App () {
-//   return (
-//     React.createElement('h1', {style : {color:"red"}}, 'App') // // <h1 style={{color: "red"}}>App</h1>
-// } 
+const Person = (props) => <h4>{props.person.name}</h4>;
 
-// const App = ()=> React.createElement('h1', {style : {color:"red"}}, 'App'); // <h1 style={{color: "red"}}>App</h1>
-
-const App = ()=> <h1 style={{color: "red"}}>App</h1>;
 export default App;
