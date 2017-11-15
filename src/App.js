1f42a233b1f01e0336c5e2e03e98931acad8c6ae
at-yenho <yen.ho@asiantech.vn>
2017-11-15 10:25:24 +0700

    way to create component and element in react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       React.createElement('h1', {style : {color:"red"}}, 'App')//<h1 style={{color: "red"}}>App</h1>
+//     );
+//   }
+// }
+
+// function App () {
+//   return (
+//     React.createElement('h1', {style : {color:"red"}}, 'App') // // <h1 style={{color: "red"}}>App</h1>
+// } 
+
+// const App = ()=> React.createElement('h1', {style : {color:"red"}}, 'App'); // <h1 style={{color: "red"}}>App</h1>
+
+const App = ()=> <h1 style={{color: "red"}}>App</h1>;
+export default App;
